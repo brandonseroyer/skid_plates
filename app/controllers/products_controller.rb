e200d2943de03daec128824fa6cff0f15313b700
Brandon Seroyer <brandonseroyer@gmail.com>
2016-10-28 16:02:43 -0700

    define variables in OrderItem Controller, all Product Controller to create OrderItem instances

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @order_item = current_order.order_items.new
   end
 
   def new
