7d8d099b4492b087918be5d6ad764230591e4488
Brandon Seroyer <brandonseroyer@gmail.com>
2016-10-28 15:50:38 -0700

    define model associations for OrderStatus and Product Classes, create controllers for Carts and OrderItems

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+  has_many :order_items
   has_many :images, dependent: :destroy
   has_attached_file :image, styles: {
     standard: '750x500',
     profile: '450x300',
     thumb: '300x225'
    }
-   
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  default_scope { where(active: true) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :home, :only => [:index]
+  resources :home, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
   resources :products do
     resources :images
   end
